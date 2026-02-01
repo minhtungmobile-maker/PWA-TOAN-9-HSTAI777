7ac65e3e106d3e0279d8b749f7316bd78e1a90f0
minhtungmobile-maker <minhtungmobile@gmail.com>
2026-02-01 20:31:14 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,59 +1,57 @@
-﻿const CACHE_NAME = "pwa-toan9-v1";
+﻿const CACHE_NAME = 'pwa-toan9-v1';
 
-const CORE_ASSETS = [
-  "/",
-  "/index.html",
-  "/offline.html",
-  "/manifest.json",
+const OFFLINE_URL = '/offline.html';
 
-  "/lessons/dai-so/ch1-can-bac-hai.html",
-  "/lessons/dai-so/ch2-ham-so-bac-nhat.html",
-  "/lessons/dai-so/ch3-he-phuong-trinh.html",
-  "/lessons/dai-so/ch4-ham-so-bac-hai.html",
+const PRECACHE_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
 
-  "/lessons/hinh-hoc/ch1-he-thuc-luong.html",
-  "/lessons/hinh-hoc/ch2-duong-tron.html",
-  "/lessons/hinh-hoc/ch3-goc-noi-tiep.html",
-
-  "/exercises/de-on-thi-vao-10-1.html",
-  "/exercises/de-on-thi-vao-10-2.html",
-  "/exercises/de-1-tiet.html",
-  "/exercises/de-15-phut.html",
-
-  "/hs-yeu/nen-tang.html",
-  "/hs-yeu/10-dang-bat-buoc.html",
-  "/hs-yeu/de-tu-5-len-9.html"
+  // folders
+  '/lessons/',
+  '/exercises/',
+  '/hs-yeu/',
 ];
 
 // INSTALL
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(PRECACHE_FILES);
+    })
   );
   self.skipWaiting();
 });
 
 // ACTIVATE
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
 });
 
 // FETCH
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return res;
-      })
-      .catch(() =>
-        caches.match(event.request).then(res => res || caches.match("/offline.html"))
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match(OFFLINE_URL)
       )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+    )
   );
 });
